7a8614d89bb71ef1032fa65ca55c3d58324eb380
Коротков Андрей Андреевич <KorotkovAA@gaz.ru>
2023-06-30 14:36:49 +0300

    v2

--- serialize/Program.cs
+++ serialize/Program.cs
@@ -7,12 +7,11 @@ using System.Text.Json.Serialization;
 
 namespace RandomData
 {
-    public class Person
+     class Person
     {
 
         public Int32 Age { get; set; }
-        public string Phone { get; set; }
-      
+        public string Phone { get; set; }      
         public string FirstName { get; private set; }               
         public Int32 Id { get; set; }
         public Guid TransportId { get; set; }
@@ -22,25 +21,22 @@ namespace RandomData
         public Int64 BirthDate { get; set; }
         public double Salary { get; set; }
         public Boolean IsMarried { get; set; }
-        public Child[]? Children { get; set; }
-
+        public List<Child> Children { get; set; }
         public Gender Gender { get; set; }
 
     }
 
 
 
-    public class Child
+     class Child
     {
         public Int32 Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public Int64 BirthDate { get; set; }
-        public Gender Gender { get; set; }
-
-        
+        public Gender Gender { get; set; }        
     }
-    public enum Gender
+     enum Gender
     {
         Male, Female
     }
@@ -51,6 +47,7 @@ namespace RandomData
         {
             Faker<Person> generatorPerson = getGeneratorPerson();           
             List<Person> persons = generatorPerson.Generate(5);
+
             PrintPersons(persons);
             Console.ReadKey();     
         }
@@ -61,6 +58,8 @@ namespace RandomData
             DateTime startDate = new DateTime(2000, 02, 6);
             DateTime endDate=new DateTime(2023, 06, 1);
             var childId = 0;
+
+            //Rebyatishki
             var Childgen = new Faker<Child>()       
        .RuleFor(c => c.Id, f => childId++)       
        .RuleFor(c => c.Gender, f => f.PickRandom<Gender>())       
@@ -69,6 +68,7 @@ namespace RandomData
       // .RuleFor(c => c.BirthDate, f => f.Date.Random(startDate, endDate));
 
             var Id = 0;
+            //Mujichki
             return new Faker<Person>("en")
 
                 .RuleFor(x => x.Id, f => Id++)
@@ -80,30 +80,27 @@ namespace RandomData
                 .RuleFor(x => x.TransportId, f => f.Random.Guid())
                 .RuleFor(x => x.Salary, f => f.Random.Int(15000, 100000))
                 //.RuleFor(c => c.BirthDate, f => f.Date.Past(dateChild, DateTime.Now))
-
                 .RuleFor(x => x.Age, f => f.Random.Byte(16, 60))
                 .RuleFor(x => x.Phone, f => f.Phone.PhoneNumber().OrNull(f, .9f))
-                .RuleFor(x => x.Salary, f => f.Random.Int(15000, 100000));
-
-                //.RuleFor(x => x.Children, f => Childgen.Generate(3).ToString().OrNull(f, 0.15f))
-                //.FinishWith((f, u) =>
-                //{
-                //    Console.WriteLine("User Created! Id={0}", u.Id);
-                //});
+                .RuleFor(x => x.Salary, f => f.Random.Int(15000, 100000))
+                .RuleFor(x => x.Children, f => Childgen.Generate(3).ToList().OrNull(f, 0.15f));
+               
 
 
         }
+     
 
-        private static void PrintPersons(List<Person> persons)
+
+            private static void PrintPersons(List<Person> persons)
         {
             foreach (var person in persons)
             {
 
                // Console.WriteLine($"Основная информация:\n{person.Id}\n{person.FirstName}\n{person.LastName}\n{person.Gender}\n{person.Age}\n{person.IsMarried}\n{person.Phone}\n{person.Salary}\n{person.TransportId}");
-                //Console.WriteLine($"Информация о работе:\n{person.Children}\n");
+               // Console.WriteLine($"Дети:\n{person.Children.Count}\n");
                 //Serialize
                 string jsonString = JsonSerializer.Serialize(person);
-                Console.WriteLine(jsonString);
+                //Console.WriteLine(jsonString);
             }
         }
     }
--- serialize/person.cs
+++ serialize/person.cs
@@ -43,59 +43,13 @@ namespace serialize
     {
         Male,Female
     }
-    class progr
-    {
-        static void Main(string[] args)
-        {
-
-            //Set the randomizer seed if you wish to generate repeatable data sets.
-            Randomizer.Seed = new Random(8675309);
-
-            var fruit = new[] { "apple", "banana", "orange", "strawberry", "kiwi" };
-
-            var orderIds = 0;
-            var testOrders = new Faker<Order>()
-                //Ensure all properties have rules. By default, StrictMode is false
-                //Set a global policy by using Faker.DefaultStrictMode
-                .StrictMode(true)
-                //OrderId is deterministic
-                .RuleFor(o => o.OrderId, f => orderIds++)
-                //Pick some fruit from a basket
-                .RuleFor(o => o.Item, f => f.PickRandom(fruit))
-                //A random quantity from 1 to 10
-                .RuleFor(o => o.Quantity, f => f.Random.Number(1, 10))
-                //A nullable int? with 80% probability of being null.
-                //The .OrNull extension is in the Bogus.Extensions namespace.
-                .RuleFor(o => o.LotNumber, f => f.Random.Int(0, 100).OrNull(f, .8f));
-
-
-            var userIds = 0;
-            var testUsers = new Faker<User>()
-                //Optional: Call for objects that have complex initialization
-                .CustomInstantiator(f => new User(userIds++, f.Random.Replace("###-##-####")))
-
-                //Use an enum outside scope.
-                .RuleFor(u => u.Gender, f => f.PickRandom<Gender>())
-
-                //Basic rules using built-in generators
-                .RuleFor(u => u.FirstName, f => f.Name.FirstName())
-                .RuleFor(u => u.LastName, f => f.Name.LastName())
-
-                //Use a method outside scope.
-                .RuleFor(u => u.CartId, f => Guid.NewGuid())
-                //Compound property with context, use the first/last name properties
-                //And composability of a complex collection.
-                .RuleFor(u => u.Orders, f => testOrders.Generate(4).ToList())
-                //Optional: After all rules are applied finish with the following action
-                .FinishWith((f, u) =>
-                {
-                    Console.WriteLine("User Created! Id={0}", u.Id);
-                });
-
-            var user = testUsers.Generate();
-            Console.WriteLine(user.DumpAsJson());
-        }
-    }
+    //class progr
+    //{
+    //    {
+    //        var user = testUsers.Generate();
+    //        Console.WriteLine(user.DumpAsJson());
+    //    }
+    //}
 }/*
     Faker<Person> generatorPerson = getGeneratorPerson();
             // генерируем 3 объекта класса Person
